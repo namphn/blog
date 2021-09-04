9e1a9bf7a447b5dca5b37a4cebbea64d1417af28
namph-uet <hoangnamuet.vnu@gmail.com>
2021-09-05 06:16:13 +0700

    feat: update API

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import { posts } from "../posts";
 import Link from "next/link";
+import React from "react";
 
 const axios = require('axios').default;
 
@@ -8,18 +9,31 @@ const getPost = async () => {
   const allPost = await axios.get("http://localhost:8082/post")
                               .then(response => {
                                 if(response.status == 200) {
-                                  console.log(response.data.data)
                                   return response.data.data
                                 }  
                               })
-                              .catch(error => {
+                             .catch(error => {
                                 return []
                               })
+
+  return allPost;
+                              
 }
 
-const Index = ({ posts }) => {
+const Index = () => {
 
-  getPost();
+  const [posts, setPost] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("http://localhost:8082/post")
+      .then(response => {
+        if(response.status == 200) {
+          setPost(response.data.data)
+        }  
+      })
+  }, [])
+
+  
 
   return (
     <Layout pageTitle="My Blog">
@@ -29,28 +43,43 @@ const Index = ({ posts }) => {
 };
 
 
-const PostsList = ({ posts }) => {
-  if (!posts || !posts.length) return <p>No posts found</p>;
-
+const PostsList = ({posts}) => {
+  if (!posts || !posts.length) return <p>Loading!</p>;
   return (
     <>
-      {posts.map((post, i) => {
-        const { frontmatter, slug } = post;
-        const { description, date, title } = frontmatter;
+      {posts.map((post) => {
+        const { id, summary, tagName, lastName, firstName, publishedAt, title, image_intro} = post;
+        let tagShow = "";
+        tagName.forEach(element => {
+          tagShow += element + ", ";
+          
+        });
+        tagShow = tagShow.slice(0, tagShow.length - 2)
+
+        // Split timestamp into [ Y, M, D, h, m, s ]
+        var t = publishedAt.split(/[- :]/);
+
+        // Apply each element to the Date function
+        var d = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
+        
 
         return (
-          <Link href={`/blog/${slug}`} key={i}>
+          <Link href={`/blog/${id}
+          
+          }`} key={id}>
             <article className="flex flex-col shadow my-4">
               <a href="#" className="hover:opacity-75">
-                <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=2" />
+                <img src={image_intro} />
               </a>
               <div className="bg-white flex flex-col justify-start p-6">
-                <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Automotive, Finance</a>
-                <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
+                <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">{
+                  tagShow
+                }</a>
+                <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">{title}</a>
                 <p href="#" className="text-sm pb-3">
-                  By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on January 12th, 2020
+                  By <a href="#" className="font-semibold hover:text-gray-800">{firstName + " " + lastName}</a>, Published on {d.toString().slice(0,15)}
                 </p>
-                <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
+                <a href="#" className="pb-6">{summary}</a>
                 <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
               </div>
             </article>
